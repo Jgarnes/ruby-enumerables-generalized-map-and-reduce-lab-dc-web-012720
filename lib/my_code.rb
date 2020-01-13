18920b7f2b63d12c08b8ed2518966b63bf0c655a
Jeremy Garnes <j06garnes@gmail.com>
2020-01-13 03:00:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,15 @@ end
 
 def reduce(source_array, starting_point = 0)
   if starting_point
-    
+    sum = starting_point
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+end 
+while i < array.length do 
+  sum = yield(sum, array[i])
+  i += 1 
+end 
+sum 
 end 
