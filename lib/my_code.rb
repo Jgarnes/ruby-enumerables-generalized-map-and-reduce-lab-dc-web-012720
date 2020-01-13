0bf75e93e48d7bc71b30148114696af65d4ca75f
Jeremy Garnes <j06garnes@gmail.com>
2020-01-13 03:02:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(array)
  new_array
 end   
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, starting_point = nil)
   if starting_point
     sum = starting_point
     i = 0 
