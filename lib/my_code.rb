b3b2fd4108f16303ee0d9fac92970ac95a0907ef
Jeremy Garnes <j06garnes@gmail.com>
2020-01-13 02:57:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,10 +9,5 @@ def map(array)
 end   
 
 def reduce(source_array, starting_point = 0)
-  total = starting_point
-  i=0 
-  while i < source_array.length do 
-    yield(total += source_array[i])
-   end
-  i += 1 
+  
 end 
